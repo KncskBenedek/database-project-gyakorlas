29b455393a69aaab31642e4514ab7108a4e7ef16
KncskBenedek <koncsikbenedek@gmail.com>
2025-09-06 18:17:05 +0200

    Custom code analysis rule

---
+++ SampleRules/SampleRules/SampleRuleConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace SampleRules
+{
+    internal static class RuleConstants
+    {
+        /// <summary>
+        /// The name of the resources file to use when looking up rule resources
+        /// </summary>
+        public const string ResourceBaseName = "SampleRules.RuleResources";
+
+        /// <summary>
+        /// Lookup name inside the resources file for the select asterisk rule name
+        /// </summary>
+        public const string AvoidWaitForDelay_RuleName = "AvoidWaitForDelay_RuleName";
+        /// <summary>
+        /// Lookup ID inside the resources file for the select asterisk description
+        /// </summary>
+        public const string AvoidWaitForDelay_ProblemDescription = "AvoidWaitForDelay_ProblemDescription";
+
+        /// <summary>
+        /// The design category (should not be localized)
+        /// </summary>
+        public const string CategoryDesign = "Design";
+
+        /// <summary>
+        /// The performance category (should not be localized)
+        /// </summary>
+        public const string CategoryPerformance = "Design";
+    }
+}
---
+++ SampleRules/SampleRules/WaitForDelayVisitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+namespace SampleRules
+{
+    internal class WaitForDelayVisitor : TSqlConcreteFragmentVisitor
+    {
+        public IList<WaitForStatement> WaitForDelayStatements { get; private set; }
+        public override void ExplicitVisit(WaitForStatement node)
+        {
+            if (node.WaitForOption == WaitForOption.Delay)
+                WaitForDelayStatements.Add(node);
+        }
+    }
+}
